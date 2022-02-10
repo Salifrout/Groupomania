ad8623ed2e37fe69d7abc45cac68ff2d3de26e8d
Salifrout <86348670+Salifrout@users.noreply.github.com>
2022-02-10 16:56:54 +0100

    installation de vue router dans le front end et création des fichiers des routes

---
+++ front-end/src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <img alt="Cette page n'existe pas" src="./assets/NotFound.png">
+    <span>Désolé, il semble que la page demandée n'existe pas. Veuillez vérifier !</span>
+</template>
+
+<style scoped>
+    img {
+        margin-top: 60px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 40px;
+        width: 80%;
+    }
+    span {
+        font-size: 80%;
+        margin-left: auto;
+        margin-right: auto;
+        font-weight: bold;
+    }
+<style>
