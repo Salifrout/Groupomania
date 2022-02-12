772af76a09a3782fea2d86d125e342f1af385448
Salifrout <86348670+Salifrout@users.noreply.github.com>
2022-02-12 19:36:49 +0100

    redémarrer vue CLI avec npm run serve

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
+<<<<<<< HEAD
 createApp(App).use(router).mount('#app')
 
 //exporte les éléments qui apparaitront dans le DOM
+=======
+createApp(App).mount('#app')
+>>>>>>> parent of ad8623e (installation de vue router dans le front end et création des fichiers des routes)
