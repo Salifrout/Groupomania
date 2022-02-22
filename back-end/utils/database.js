77cffbdebf763a2f01e9cab465b17a9de2ab4596
Salifrout <86348670+Salifrout@users.noreply.github.com>
2022-02-22 04:17:18 +0100

    ajout du header et du footer dans app.vue (front-end)

--- back-end/utils/database.js
+++ back-end/utils/database.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(
         pool: {
             max: 5,
             min: 0,
+            acquire: 30000,
             idle: 10000
         }
     }
--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,20 +1,66 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
+<header>
+  <!--<div id="topBar">-->
+    <div id="logoBar">
+      <h1 id="groupomania">Groupomania</h1>
+    </div>
+    <!--<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>-->
+    <div id="navBar"></div>
+  <!--</div>-->
+</header>
+
+<footer>
+  <div id="botBar">
+
   </div>
+</footer>
+
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}*/
+
+header {
+  display: flex;
+  margin-bottom: 160px;
+  #logoBar {
+    background-color: #fd2d01;
+    height: 120px;
+    width: 70%;
+    & h1 {
+      color: #ffd7d7;
+      font-weight: 900;
+      margin: auto;
+      padding: 40px 35%;
+    }
+  }  
+  #navBar {
+    background-color: #ffd7d7;
+    color: #fd2d01;
+    text-decoration: underline;
+    width: 30%;
+    height: 120px;
+  }
 }
 
+footer {
+  margin: 0;
+  margin-top: 160px;
+  background-color: #fd2d01;
+  height: 80px;
+}
+/*
 #nav {
   padding: 30px;
 
@@ -26,5 +72,5 @@
       color: #42b983;
     }
   }
-}
+}*/
 </style>
